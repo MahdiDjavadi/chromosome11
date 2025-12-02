0e1ac02cac9ddf2a985b7a328fa4744bff3c20ec
Mahdi <javadi.edu@gmail.com>
2025-12-02 17:03:30 +0330

    set valid againt header

--- src/fetch_br_api.py
+++ src/fetch_br_api.py
@@ -12,9 +12,10 @@ TYPE = 0  # یا 1
 URL = f"https://brsapi.ir/Api/Tsetmc/History.php?key={API_KEY}&type={TYPE}&l18={SYMBOL}"
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/142.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/131.0.0.0 Safari/537.36 OPR/106.0.0.0",
+        "Accept": "application/json, text/plain, */*"
 }
 
 def fetch_and_insert():
